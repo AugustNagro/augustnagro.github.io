987ac71bdcbc7fad11e2a6a5cbf060d1840c6925
August Nagro <augustnagro@gmail.com>
2016-07-24 17:57:24 -0500

    updated project posts

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-(function() {
+document.addEventListener('DOMContentLoaded', function(){
     var gifLoadTimeMs = 4.19 * 1000;
     var container = document.body;
 
@@ -6,16 +6,18 @@
         var animViewDay = localStorage.getItem("animViewDay");
         var currentDate = new Date().getDay();
 
+        // allows the cached, single-loop gif to replay on page reload
         var gif = document.getElementById('gif');
         var oldSrc = gif.getAttribute('src');
         gif.src = "";
         gif.src = oldSrc + "?" + (new Date()).getTime();
+
         container.classList.add('loading');
 
         if(animViewDay != null && currentDate - animViewDay == 0){
-            document.getElementsByClassName("ip-header")[0].style.background = "#fff";
             startLoading(0);
         }else{
+            document.getElementsByClassName("ip-header")[0].style.background = "#00023D";
             startLoading(gifLoadTimeMs);
         }
 
@@ -29,9 +31,9 @@
 
             var headerText = document.getElementById('header-text');
             headerText.classList.remove('u-hide');
-            
+
         }, timeout);
     }
 
     init();
-})();
+}, false)
